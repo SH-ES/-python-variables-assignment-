7ce79dc6963ba02d86297946318bdb93b2c0adab
SH-ES <shahina.hussains@gmail.com>
2025-05-09 02:25:00 +0500

    Add files via upload

---
+++ variables_practice.py
@@ -0,0 +1,15 @@
+Part 1 Declare Variables
+name = "Shahina Hussain"
+Age = 51
+course-name = "Python Basics"
+country = "Pakistan"
+
+Part 2  Invalid Variable Examples (Commented) 
+1name = "Shahina Hussain" #❌Variable can not start with a number
+my-name = "Shahina Hussain" #❌Variable can not have a hiphen/dash
+user name = "Shahina Hussain" #❌Variable can not have a space
+
+Part 3 Apply Best Practices
+student_name = "Ahmed"
+age = 18
+total_marks = 95
